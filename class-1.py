4a5360507cb7c4c0336df91a9b72f60a562dfe80
cyberspyde <cyberspyde@gmail.com>
2024-11-27 10:17:18 +0500

    update

---
+++ class-1.py
@@ -0,0 +1,14 @@
+#Basic Syntax, bu kommentariya
+print("Hello world")
+
+# Python kod blocklarni belgilash uchun indentatsiyadan foydalanadi
+def func():
+    print("hello world")
+
+# data types and Variables
+name = "Alice"  # String
+age = 25        # Integer
+height = 5.6    # Float
+is_student = True  # Boolean
+print(name, age, height, is_student)
+
